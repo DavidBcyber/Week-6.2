436d6d9f446d7b6fabe27ce247508642c94f4636
DavidBcyber <dbustos28@amarillocollege.com>
2025-05-04 21:36:20 -0500

    Update Program.cs

--- 6.2/Program.cs
+++ 6.2/Program.cs
@@ -6,23 +6,22 @@ using System.Threading.Tasks;
 
 namespace _6._2
 {
-    class Student // type of class
+    class Student // The student class information
     {
-        // public strings and ints
+        // public strings and int for the student info
         public string name { get; set; }
         public int ID { get; set; }
         public int Age { get; set; }
         public string Course { get; set; }
 
-        public Student(string name, int ID, int Age, string Course)
+        public Student(string name, int ID, int Age, string Course) // the class infomation to identify to put in the system
         {
             this.name = name;
             this.ID = ID;
             this.Age = Age;
             this.Course = Course;
         }
-        //Display Info
-        public virtual void DisplayInfo()
+        public virtual void DisplayInfo() // The display for every student information
         {
             Console.WriteLine($" {name} -- {ID} -- {Age} -- {Course} ");
         }
@@ -32,14 +31,15 @@ namespace _6._2
     {
         static void Main(string[] args)
         {
-            //Students Info
+            //The first student info for Jeff
             Student student1 = new Student("Jeff", 2589856, 10, "Math");
             student1.DisplayInfo();
 
-
+            //The second student's info for Bob
             Student student2 = new Student("Bob", 7826984, 11, "History");
             student2.DisplayInfo();
-
+            
+            //The third student's info for Mike
             Student student3 = new Student("Mike", 4589896, 12, "Science");
             student3.DisplayInfo();
         }
